8db7ecd056a582fd733160cc789efcc9b9a1364c
Marcel Zięba <marseel@gmail.com>
2014-11-16 18:24:16 +0100

    Added pylint and test framework

---
+++ deltacompression/storage_test.py
@@ -0,0 +1,16 @@
+"""Tests for storage.py."""
+
+import unittest
+
+from deltacompression import storage
+
+
+class StorageTest(unittest.TestCase):
+  """Tests for Storage class."""
+
+  def test_addChunk(self):
+    pass
+
+
+if __name__ == '__main__':
+  unittest.main()
