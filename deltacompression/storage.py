f228f06f8772c6146d4fc9853583371bcd57d4c3
Marcel Zięba <marseel@gmail.com>
2014-11-17 23:32:31 +0100

    Fixed indents

--- deltacompression/storage.py
+++ deltacompression/storage.py
@@ -2,46 +2,46 @@
 
 
 class Storage(object):
-  """Class responsible for managing data chunks."""
+    """Class responsible for managing data chunks."""
 
-  def __init__(self, hash_function, logger):
-    """Creates Storage object.
+    def __init__(self, hash_function, logger):
+        """Creates Storage object.
 
-    Args:
-      hash_function: instance of HashFunction.
-      logger: instance of DriveLogger.
-    """
+        Args:
+            hash_function: instance of HashFunction.
+            logger: instance of DriveLogger.
+        """
 
-  def getChunk(self, hash_value):
-    """Retrieves Chunk.
+    def getChunk(self, hash_value):
+        """Retrieves Chunk.
 
-    Args:
-      hash_value: hash value of chunk.
-    Returns:
-      instance of Chunk corresponding to given hash.
-    """
+        Args:
+            hash_value: hash value of chunk.
+        Returns:
+            instance of Chunk corresponding to given hash.
+        """
 
 
-  def addChunk(self, chunk):
-    """Adds new chunk to storage.
+    def addChunk(self, chunk):
+        """Adds new chunk to storage.
 
-    Args:
-      chunk: instance of Chunk.
-    Returns:
-      hash of given chunk.
-    """
+        Args:
+            chunk: instance of Chunk.
+        Returns:
+            hash of given chunk.
+        """
 
 
 class Chunk(object):
-  """Class responsible for storing one data chunk."""
+    """Class responsible for storing one data chunk."""
 
-  def __init__(self, data):
-    """Creates data chunk.
+    def __init__(self, data):
+        """Creates data chunk.
 
-    Args:
-      data: chunk of data.
-    """
-    self._data = data
+        Args:
+            data: chunk of data.
+        """
+        self._data = data
 
-  def get(self):
-    return self._data
+    def get(self):
+        return self._data
--- deltacompression/storage_test.py
+++ deltacompression/storage_test.py
@@ -8,52 +8,52 @@ from deltacompression import storage
 # TODO(marseel): should be derivied class of HashFunction
 class SimpleHasher(object):
 
-  def computeHash(self, chunk):
-    """Computes hash.
+    def computeHash(self, chunk):
+        """Computes hash.
 
-    Args:
-      chunk: string
-    Returns:
-      Integer"""
-    return sum(map(ord, chunk))
+        Args:
+            chunk: string
+        Returns:
+            Integer"""
+        return sum(map(ord, chunk))
 
 
 class StorageTest(unittest.TestCase):
-  """Tests for Storage class."""
+    """Tests for Storage class."""
 
-  def setUp(self):
-    self._hasher = SimpleHasher()
-    self._storage = storage.Storage(self._hasher, None)
+    def setUp(self):
+        self._hasher = SimpleHasher()
+        self._storage = storage.Storage(self._hasher, None)
 
-  def testAddChunk(self):
-    chunk1 = "aaa"
-    chunk2 = "bbb"
-    chunk1_hash = self._hasher.computeHash(chunk1)
-    chunk2_hash = self._hasher.computeHash(chunk2)
-    returned_hash1 = self._storage.addChunk(chunk1)
-    returned_hash2 = self._storage.addChunk(chunk2)
+    def testAddChunk(self):
+        chunk1 = "aaa"
+        chunk2 = "bbb"
+        chunk1_hash = self._hasher.computeHash(chunk1)
+        chunk2_hash = self._hasher.computeHash(chunk2)
+        returned_hash1 = self._storage.addChunk(chunk1)
+        returned_hash2 = self._storage.addChunk(chunk2)
 
-    self.assertEqual(chunk1_hash, returned_hash1)
-    self.assertEqual(chunk2_hash, returned_hash2)
+        self.assertEqual(chunk1_hash, returned_hash1)
+        self.assertEqual(chunk2_hash, returned_hash2)
 
-  def testGetChunk(self):
-    chunk1 = "aaa"
-    chunk2 = "bbb"
-    hash1 = self._storage.addChunk(chunk1)
-    hash2 = self._storage.addChunk(chunk2)
+    def testGetChunk(self):
+        chunk1 = "aaa"
+        chunk2 = "bbb"
+        hash1 = self._storage.addChunk(chunk1)
+        hash2 = self._storage.addChunk(chunk2)
 
-    self.assertEqual("aaa", self._storage.getChunk(hash1))
-    self.assertEqual("bbb", self._storage.getChunk(hash2))
+        self.assertEqual("aaa", self._storage.getChunk(hash1))
+        self.assertEqual("bbb", self._storage.getChunk(hash2))
 
 
 class ChunkTest(unittest.TestCase):
 
-  def testGet(self):
-    chunk = storage.Chunk("asd")
-    self.assertTrue(chunk.get(), "asd")
+    def testGet(self):
+        chunk = storage.Chunk("asd")
+        self.assertTrue(chunk.get(), "asd")
 
-    chunk2 = storage.Chunk("dsa")
-    self.assertTrue(chunk2.get(), "dsa")
+        chunk2 = storage.Chunk("dsa")
+        self.assertTrue(chunk2.get(), "dsa")
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
