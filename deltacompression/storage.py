edc6207a44388bda54691420da726072d7309563
Marcel Zięba <marseel@gmail.com>
2014-11-17 23:14:00 +0100

    Fixed indents

--- deltacompression/storage.py
+++ deltacompression/storage.py
@@ -2,59 +2,59 @@
 
 
 class StorageException(Exception):
-  """Exception thrown by Storage class."""
+    """Exception thrown by Storage class."""
 
 
 class Storage(object):
-  """Class responsible for managing data chunks."""
+    """Class responsible for managing data chunks."""
 
-  def __init__(self, hash_function, logger):
-    """Creates Storage object.
+    def __init__(self, hash_function, logger):
+        """Creates Storage object.
 
-    Args:
-      hash_function: instance of HashFunction.
-      logger: instance of DriveLogger.
-    """
-    self._hash_function = hash_function
-    self._logger = logger
-    self._storage = {}
+        Args:
+            hash_function: instance of HashFunction.
+            logger: instance of DriveLogger.
+        """
+        self._hash_function = hash_function
+        self._logger = logger
+        self._storage = {}
 
-  def getChunk(self, hash_value):
-    """Retrieves Chunk.
+    def getChunk(self, hash_value):
+        """Retrieves Chunk.
 
-    Args:
-      hash_value: hash value of chunk.
-    Returns:
-      instance of Chunk corresponding to given hash.
-    Raises:
-      StorageException if chunk does not exist.
-    """
-    if hash_value not in self._storage:
-      raise StorageException("Chunk does not exist.")
-    return self._storage[hash_value]
+        Args:
+            hash_value: hash value of chunk.
+        Returns:
+            instance of Chunk corresponding to given hash.
+        Raises:
+            StorageException if chunk does not exist.
+        """
+        if hash_value not in self._storage:
+            raise StorageException("Chunk does not exist.")
+        return self._storage[hash_value]
 
-  def containsHash(self, hash_value):
-    """Checks if storage contains chunk with given hash_value.
+    def containsHash(self, hash_value):
+        """Checks if storage contains chunk with given hash_value.
 
-    Args:
-      hash_value: hash of chunk to check.
-    Returns:
-      True if contains hash and False if not.
-    """
-    return hash_value in self._storage
+        Args:
+            hash_value: hash of chunk to check.
+        Returns:
+            True if contains hash and False if not.
+        """
+        return hash_value in self._storage
 
-  def addChunk(self, chunk):
-    """Adds new chunk to storage.
+    def addChunk(self, chunk):
+        """Adds new chunk to storage.
 
-    Args:
-      chunk: instance of Chunk.
-    Returns:
-      hash of given chunk.
-    Raises:
-      StorageException if chunk already exists
-    """
-    hash_value = self._hash_function.computeHash(chunk)
-    if hash_value in self._storage:
-      raise StorageException("Chunk with given hash already exists.")
-    self._storage[hash_value] = chunk
-    return hash_value
+        Args:
+            chunk: instance of Chunk.
+        Returns:
+            hash of given chunk.
+        Raises:
+            StorageException if chunk already exists
+        """
+        hash_value = self._hash_function.computeHash(chunk)
+        if hash_value in self._storage:
+            raise StorageException("Chunk with given hash already exists.")
+        self._storage[hash_value] = chunk
+        return hash_value
--- deltacompression/storage_test.py
+++ deltacompression/storage_test.py
@@ -8,55 +8,55 @@ from deltacompression import storage
 # TODO(marseel): should be derivied class of HashFunction
 class SimpleHasher(object):
 
-  def computeHash(self, chunk):
-    """Computes hash.
+    def computeHash(self, chunk):
+        """Computes hash.
 
-    Args:
-      chunk: string
-    Returns:
-      Integer"""
-    return sum(map(ord, chunk))
+        Args:
+            chunk: string
+        Returns:
+            Integer"""
+        return sum(map(ord, chunk))
 
 
 class StorageTest(unittest.TestCase):
-  """Tests for Storage class."""
+    """Tests for Storage class."""
 
-  def setUp(self):
-    self._hasher = SimpleHasher()
-    self._storage = storage.Storage(self._hasher, None)
+    def setUp(self):
+        self._hasher = SimpleHasher()
+        self._storage = storage.Storage(self._hasher, None)
 
-  def testAddChunk(self):
-    chunk1 = "aaa"
-    chunk2 = "bbb"
-    chunk1_hash = self._hasher.computeHash(chunk1)
-    chunk2_hash = self._hasher.computeHash(chunk2)
-    returned_hash1 = self._storage.addChunk(chunk1)
-    returned_hash2 = self._storage.addChunk(chunk2)
+    def testAddChunk(self):
+        chunk1 = "aaa"
+        chunk2 = "bbb"
+        chunk1_hash = self._hasher.computeHash(chunk1)
+        chunk2_hash = self._hasher.computeHash(chunk2)
+        returned_hash1 = self._storage.addChunk(chunk1)
+        returned_hash2 = self._storage.addChunk(chunk2)
 
-    self.assertEqual(chunk1_hash, returned_hash1)
-    self.assertEqual(chunk2_hash, returned_hash2)
+        self.assertEqual(chunk1_hash, returned_hash1)
+        self.assertEqual(chunk2_hash, returned_hash2)
 
-  def testGetChunk(self):
-    chunk1 = "aaa"
-    chunk2 = "bbb"
-    hash1 = self._storage.addChunk(chunk1)
-    hash2 = self._storage.addChunk(chunk2)
+    def testGetChunk(self):
+        chunk1 = "aaa"
+        chunk2 = "bbb"
+        hash1 = self._storage.addChunk(chunk1)
+        hash2 = self._storage.addChunk(chunk2)
 
-    self.assertEqual("aaa", self._storage.getChunk(hash1))
-    self.assertEqual("bbb", self._storage.getChunk(hash2))
+        self.assertEqual("aaa", self._storage.getChunk(hash1))
+        self.assertEqual("bbb", self._storage.getChunk(hash2))
 
-  def testAddChunkRaisesWhenChunkExists(self):
-    self._storage.addChunk("aaa")
-    self.assertRaises(storage.StorageException, self._storage.addChunk, "aaa")
+    def testAddChunkRaisesWhenChunkExists(self):
+        self._storage.addChunk("aaa")
+        self.assertRaises(storage.StorageException, self._storage.addChunk, "aaa")
 
-  def testGetChunkRaisesWhenChunkDoesNotExist(self):
-    self.assertRaises(storage.StorageException, self._storage.getChunk, 123)
+    def testGetChunkRaisesWhenChunkDoesNotExist(self):
+        self.assertRaises(storage.StorageException, self._storage.getChunk, 123)
 
-  def testContainsHash(self):
-    hash1 = self._storage.addChunk("aaa")
-    self.assertTrue(self._storage.containsHash(hash1))
-    self.assertFalse(self._storage.containsHash(123))
+    def testContainsHash(self):
+        hash1 = self._storage.addChunk("aaa")
+        self.assertTrue(self._storage.containsHash(hash1))
+        self.assertFalse(self._storage.containsHash(123))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
