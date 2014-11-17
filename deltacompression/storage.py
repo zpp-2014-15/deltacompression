9b6e39c3070755b2df4fed3c4dee98d886d532bd
pawkura <paw.kura@gmail.com>
2014-11-17 14:53:54 +0100

    Fixed indents

--- deltacompression/storage.py
+++ deltacompression/storage.py
@@ -2,31 +2,31 @@
 
 
 class Storage(object):
-  """Class responsible for managing data chunks."""
+    """Class responsible for managing data chunks."""
 
-  def __init__(self, hash_function, logger):
-    """Creates Storage object.
+    def __init__(self, hash_function, logger):
+        """Creates Storage object.
 
-    Args:
-      hash_function: instance of HashFunction.
-      logger: instance of DriveLogger.
-    """
+        Args:
+            hash_function: instance of HashFunction.
+            logger: instance of DriveLogger.
+        """
 
-  def getChunk(self, hash_value):
-    """Retrieves Chunk.
+    def getChunk(self, hash_value):
+        """Retrieves Chunk.
 
-    Args:
-      hash_value: hash value of chunk.
-    Returns:
-      instance of Chunk corresponding to given hash.
-    """
+        Args:
+            hash_value: hash value of chunk.
+        Returns:
+            instance of Chunk corresponding to given hash.
+        """
 
 
-  def addChunk(self, chunk):
-    """Adds new chunk to storage.
+    def addChunk(self, chunk):
+        """Adds new chunk to storage.
 
-    Args:
-      chunk: instance of Chunk.
-    Returns:
-      hash of given chunk.
-    """
+        Args:
+            chunk: instance of Chunk.
+        Returns:
+            hash of given chunk.
+        """
