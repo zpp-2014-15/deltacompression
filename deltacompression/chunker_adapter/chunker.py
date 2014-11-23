0c34b4090ed9cb35d2aa22bfc1d03b3cc285fd98
Dawid Lazarczyk <dl337614@students.mimuw.edu.pl>
2014-11-23 21:52:27 +0100

    Added test to chunker and fixes after review

--- deltacompression/chunker_adapter/chunker.py
+++ deltacompression/chunker_adapter/chunker.py
@@ -1,17 +1,21 @@
 """Python adapter for the external chunking module."""
 
+import os.path as op
 from subprocess import Popen, PIPE
 import cStringIO
+
 from deltacompression.backend.storage import Chunk
-import os.path as op
+
 
 class ChunkerException(Exception):
     """Exception thrown by Chunker."""
+
 
 class Chunker(object):
     """Python adapter for the external chunker module."""
 
     path = op.join(op.abspath(op.dirname(__file__)), 'adapter')
+    no_file_msg = "There is no '{}' file"
 
     def __init__(self, min_chunk, max_chunk):
         """Creates a Chunker object.
@@ -23,7 +27,7 @@ class Chunker(object):
             ChunkerException, if the needed adapter binary is not present.
         """
         if not op.isfile(self.path):
-            raise ChunkerException("There is no '{}' file".format(self.path))
+            raise ChunkerException(self.no_file_msg.format(self.path))
         self._min_chunk = min_chunk
         self._max_chunk = max_chunk
 
@@ -39,6 +43,9 @@ class Chunker(object):
             OSError
             IOError
         """
+        if not op.isfile(file_name):
+            raise ChunkerException(self.no_file_msg.format(file_name))
+
         process = Popen([self.path, str(self._min_chunk),
                          str(self._max_chunk), file_name], stdout=PIPE)
         out, err = process.communicate()
--- deltacompression/chunker_adapter/chunker_test.py
+++ deltacompression/chunker_adapter/chunker_test.py
@@ -1,23 +1,24 @@
 """Tests for chunker.py"""
 
-import unittest
-from deltacompression.chunker_adapter.chunker import Chunker
 import os
 import os.path as op
+import unittest
+
+from deltacompression.chunker_adapter.chunker import Chunker, ChunkerException
+
 
 class ChunkerTest(unittest.TestCase):
     """ Test for the Chunker class. """
 
     file_name = op.join(op.abspath(op.dirname(__file__)), '__test_file__')
-
-    def __init__(self, *args, **kwargs):
-        super(ChunkerTest, self).__init__(*args, **kwargs)
-        self._chunker = None
+    bad_file_name = "___surely_there_is_no_such_file___"
 
     def setUp(self):
         self._chunker = Chunker(1000, 7000)
 
     def testChunking(self):
+        # due to chunker module's limitations we can't actually test it on
+        # smaller data
         with open(self.file_name, 'w') as tfile:
             cont = ",".join([str(i) for i in xrange(15000)])
             tfile.write(cont)
@@ -28,6 +29,14 @@ class ChunkerTest(unittest.TestCase):
         finally:
             os.remove(self.file_name)
         self.assertEqual(cont, ncont)
+
+    def testBadFile(self):
+        """Testing chunker's behaviour after passing nonexistent file."""
+        try:
+            ret = [i for i in self._chunker.chunkData(self.bad_file_name)]
+            self.assertFalse(ret)
+        except ChunkerException:
+            pass
 
 
 if __name__ == '__main__':
