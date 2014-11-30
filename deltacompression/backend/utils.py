18381e80b90e7737343891a8995a85d08116580e
Dawid Lazarczyk <dl337614@students.mimuw.edu.pl>
2014-11-30 13:58:35 +0100

    Implemented optimal delta compression algorithm

---
+++ deltacompression/backend/utils.py
@@ -0,0 +1,49 @@
+"""Classes used in many other modules - e.g. mockup classes for tests."""
+
+import struct
+
+from deltacompression.backend import diff_algorithm
+from deltacompression.backend import storage
+from deltacompression.backend import chunk_hash
+
+
+class MockupDiff(diff_algorithm.DiffAlgorithm):
+    """Dummy diff algorithm which just doesn't use the base chunk."""
+
+    def calculateDiff(self, base_chunk, new_chunk):
+        return new_chunk.get()
+
+    def applyDiff(self, base_chunk, diff):
+        return storage.Chunk(diff)
+
+
+class PrefixDiff(diff_algorithm.DiffAlgorithm):
+    """A diff algorithm which compares prefixes of chunks."""
+
+    FMT = "<i"
+    SIZE = struct.calcsize(FMT)
+
+    def calculateDiff(self, base_chunk, new_chunk):
+        prefix = 0
+        base = base_chunk.get()
+        new = new_chunk.get()
+        mini = min(len(base), len(new))
+        while prefix < mini and base[prefix] == new[prefix]:
+            prefix += 1
+        return struct.pack(self.FMT, prefix) + new[prefix:]
+
+    def applyDiff(self, base_chunk, diff):
+        prefix, = struct.unpack(self.FMT, diff[:self.SIZE])
+        rest = diff[self.SIZE:]
+        return storage.Chunk(base_chunk.get()[:prefix] + rest)
+
+
+class PrefixHash(chunk_hash.HashFunction):
+
+    LEN = 10
+
+    def calculateHash(self, chunk):
+        return chunk.get()[:self.LEN].zfill(self.LEN)
+
+    def getHashSize(self):
+        return self.LEN
