75c95602ef79ddee77fd5202e0c12554a30af71c
trzysiek <trzysiek@gmail.com>
2015-03-01 22:48:58 +0100

    A really alpha version

--- deltacompression/backend/storage.py
+++ deltacompression/backend/storage.py
@@ -1,11 +1,14 @@
 """Module contains all classes responsible for storing data."""
 
+from pymongo import MongoClient
+from bson.binary import Binary
+
 
 class StorageException(Exception):
     """Exception thrown by Storage class."""
 
 
-class Storage(object):
+class BStorage(object):
     """Class responsible for managing data chunks."""
 
     def __init__(self, hash_function, logger):
@@ -17,7 +20,6 @@ class Storage(object):
         """
         self._hash_function = hash_function
         self._logger = logger
-        self._storage = {}
 
     def getChunk(self, hash_value):
         """Retrieves Chunk.
@@ -29,9 +31,7 @@ class Storage(object):
         Raises:
             StorageException if chunk does not exist.
         """
-        if hash_value not in self._storage:
-            raise StorageException("Chunk does not exist.")
-        return self._storage[hash_value]
+        return NotImplementedError
 
     def containsHash(self, hash_value):
         """Checks if storage contains chunk with given hash_value.
@@ -41,7 +41,7 @@ class Storage(object):
         Returns:
             True if contains hash and False if not.
         """
-        return hash_value in self._storage
+        return NotImplementedError
 
     def addChunk(self, chunk):
         """Adds new chunk to storage.
@@ -53,18 +53,14 @@ class Storage(object):
         Raises:
             StorageException if chunk already exists.
         """
-        hash_value = self._hash_function.calculateHash(chunk)
-        if hash_value in self._storage:
-            raise StorageException("Chunk with given hash already exists.")
-        self._storage[hash_value] = chunk
-        return hash_value
+        return NotImplementedError
 
     def containsChunk(self, chunk):
         """
         Returns:
             true if chunk is in the storage, false otherwise.
         """
-        return self._hash_function.calculateHash(chunk) in self._storage
+        return NotImplementedError
 
     def getHashOfChunk(self, chunk):
         """
@@ -75,20 +71,97 @@ class Storage(object):
         Raises:
             StorageException if chunk is not in the storage.
         """
-        hash_value = self._hash_function.calculateHash(chunk)
-        if hash_value not in self._storage:
-            raise StorageException("Given chunk does not exist in the storage.")
-        return hash_value
+        return NotImplementedError
 
     def getChunks(self):
         """
         Returns:
             An iterable for all the contained Chunks.
         """
-        return self._storage.viewvalues()
+        return NotImplementedError
 
     def getHashFunction(self):
         return self._hash_function
+
+
+class RAMStorage(BStorage):
+    """Class responsible for managing data chunks."""
+
+    def __init__(self, hash_function, logger):
+        self._storage = {}
+
+    def getChunk(self, hash_value):
+        if hash_value not in self._storage:
+            raise StorageException("Chunk does not exist.")
+        return self._storage[hash_value]
+
+    def containsHash(self, hash_value):
+        return hash_value in self._storage
+
+    def addChunk(self, chunk):
+        hash_value = self._hash_function.calculateHash(chunk)
+        if hash_value in self._storage:
+            raise StorageException("Chunk with given hash already exists.")
+        self._storage[hash_value] = chunk
+        return hash_value
+
+    def containsChunk(self, chunk):
+        return self._hash_function.calculateHash(chunk) in self._storage
+
+    def getHashOfChunk(self, chunk):
+        hash_value = self._hash_function.calculateHash(chunk)
+        if hash_value not in self._storage:
+            raise StorageException("Given chunk does not exist in the storage.")
+        return hash_value
+
+    def getChunks(self):
+        return self._storage.viewvalues()
+
+
+class Storage(BStorage):
+
+    def __init__(self, hash_function, logger):
+        super(Storage, self).__init__(hash_function, logger)
+        self._client = MongoClient()
+        self._db = self._client['storage-db']
+        self._storage = self._db['storage-collection']
+        self._hashes = set()
+
+    def _addToDB(self, hash_value, chunk):
+        self._storage.insert({'hash': Binary(hash_value), 'chunk': chunk.get()})
+
+    def _getFromDB(self, hash_value):
+        aa = self._storage.find_one({'hash': Binary(hash_value)})
+        return Chunk(aa['chunk'])
+
+    def getChunk(self, hash_value):
+        if hash_value not in self._hashes:
+            raise StorageException("Chunk does not exist.")
+        return _getFromDB(hash_value)
+
+    def containsHash(self, hash_value):
+        return hash_value in self._hashes
+
+    def addChunk(self, chunk):
+        hash_value = self._hash_function.calculateHash(chunk)
+        if hash_value in self._hashes:
+            raise StorageException("Chunk with given hash already exists.")
+        self._addToDB(hash_value, chunk)
+        self._hashes.add(hash_value)
+        return hash_value
+
+    def containsChunk(self, chunk):
+        return self._hash_function.calculateHash(chunk) in self._hashes
+
+    def getHashOfChunk(self, chunk):
+        hash_value = self._hash_function.calculateHash(chunk)
+        if hash_value not in self._hashes:
+            raise StorageException("Given chunk does not exist in the storage.")
+        return hash_value
+
+    def getChunks(self):
+        for h in self._hashes:
+            yield self._getFromDB(h)
 
 
 class Chunk(object):
