57c685e047a091bdd8c7bf26460e4e22639593d2
trzysiek <trzysiek@gmail.com>
2015-03-01 12:33:30 +0100

    Added lzma to factory

--- deltacompression/backend/compression_factory.py
+++ deltacompression/backend/compression_factory.py
@@ -8,11 +8,13 @@ class CompressionFactory(object):
     DUMMY_COMPRESSION = "No compression"
     LZO_COMPRESSION = "Lzo compression"
     ZIP_COMPRESSION = "Zip compression"
+    LZMA_COMPRESSION = "Lzma compression"
 
     ALL_COMPRESSIONS = [
         DUMMY_COMPRESSION,
         LZO_COMPRESSION,
-        ZIP_COMPRESSION
+        ZIP_COMPRESSION,
+        LZMA_COMPRESSION
     ]
 
     def getCompressions(self):
@@ -25,3 +27,5 @@ class CompressionFactory(object):
             return compression.LzoCompression()
         elif name == self.ZIP_COMPRESSION:
             return compression.ZipCompression()
+        elif name == self.LZMA_COMPRESSION:
+            return compression.LzmaCompression()
