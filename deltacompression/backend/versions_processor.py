a849007727a48002033a69c1d92de18ab21e1e88
Paweł Kura <paw.kura@gmail.com>
2015-03-24 21:41:59 +0100

    Style fix

--- deltacompression/backend/versions_processor.py
+++ deltacompression/backend/versions_processor.py
@@ -15,6 +15,10 @@ class VersionsProcessor(object):
     def setDirectoryProcessor(self, directory_processor):
         self._directory_processor = directory_processor
 
+    def _natural_sort_key(self, (_, directory)):
+        return [int(s) if s.isdigit() else s
+                for s in re.split(r'(\d+)', directory)]
+
     def runSimulation(self, directory):
         """Processes directory with versions as subdirectories.
 
@@ -29,10 +33,8 @@ class VersionsProcessor(object):
         """
         all_files = [(op.join(directory, file_name), file_name)
                      for file_name in os.listdir(directory)]
-        natural_sort_key = lambda (_, dir): [int(s) if s.isdigit() else s
-                                             for s in re.split(r'(\d+)', dir)]
         all_dirs = sorted([(x, y) for (x, y) in all_files if op.isdir(x)],
-                          key=natural_sort_key)
+                          key=self._natural_sort_key)
         logger = self._directory_processor.getLogger()
 
         for full_path, version_dir in all_dirs:
