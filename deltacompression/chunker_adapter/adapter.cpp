cc1763007cf1b80f6715f70a3853734bffc3d158
Dawid Lazarczyk <dl337614@students.mimuw.edu.pl>
2015-03-26 13:14:57 +0100

    Modified the chunker adapter

--- deltacompression/chunker_adapter/adapter.cpp
+++ deltacompression/chunker_adapter/adapter.cpp
@@ -1,17 +1,64 @@
 #include "chunker.h"
 #include "cool/types.h"
 #include <memory>
+#include <cassert>
+#include <list>
+#include <utility>
 #include <fstream>
 #include <streambuf>
 #include <string>
 #include <algorithm>
 #include <iostream>
 
-size_t getChunkSize(const cool::IoVec &chunk) {
-    return chunk[0].iov_len;
-}
 
-int processFile(const std::string &fileName, hydra::chunking::ChunkerAdapter &adapter) {
+class ChunkerAdapter {
+private:
+    std::list<std::pair<std::shared_ptr<char>, size_t>> regions;
+    hydra::chunking::ChunkerAdapter adapter;
+    void removeChunk(size_t ch_size) {
+        while (ch_size) {
+            auto && region = regions.front();
+            if (region.second > ch_size) {
+                region.second -= ch_size;
+                ch_size = 0;
+            }
+            else {
+                ch_size -= region.second;
+                regions.pop_front();
+            }
+        }
+    }
+
+public:
+    ChunkerAdapter(int minChunk, int maxChunk, int avgChunk)
+        : adapter(minChunk, maxChunk, avgChunk) {}
+
+    void addMemoryRegion(const std::shared_ptr<char> &cont, size_t size) {
+        cool::IoVec data;
+        data.push_back(cont.get(), size);
+        adapter.add(data);
+        regions.push_back(std::make_pair(cont, size));
+    }
+
+    bool haveChunk() { return adapter.haveChunk(); }
+    bool isEmpty() { return adapter.isEmpty(); }
+    size_t getChunk() {
+        assert(haveChunk());
+        size_t bytes = adapter.getChunkRef().getTotalBytes();
+        removeChunk(bytes);
+        return bytes;
+    }
+
+    size_t getTerminalChunk() {
+        assert(!isEmpty());
+        size_t bytes = adapter.getTerminalChunkRef().getTotalBytes();
+        removeChunk(bytes);
+        return bytes;
+    }
+};
+
+
+int processFile(const std::string &fileName, ChunkerAdapter &adapter) {
     std::ifstream in(fileName, std::ios::binary | std::ios::in);
     if (!in) {
         throw "Failed to open the file.";
@@ -23,7 +70,7 @@ int processFile(const std::string &fileName, hydra::chunking::ChunkerAdapter &ad
 
     size_t content_size = in.tellg();
     if (content_size != 0) {
-        std::unique_ptr<char[]> content(new char[content_size]);
+        std::shared_ptr<char> content(new char[content_size]);
 
         in.seekg(0, std::ios::beg);
 
@@ -31,14 +78,11 @@ int processFile(const std::string &fileName, hydra::chunking::ChunkerAdapter &ad
                   std::istreambuf_iterator<char>(),
                   content.get());
         in.close();
-        cool::IoVec data;
-        data.push_back(content.get(), content_size);
-        adapter.add(data);
 
+        adapter.addMemoryRegion(content, content_size);
         // we'll be only writing sizes of consecutive chunks to output
-        while (adapter.haveChunk()) {
-            std::cout << getChunkSize(adapter.getChunkRef()) << std::endl;
-        }
+        while (adapter.haveChunk())
+            std::cout << adapter.getChunk() << std::endl;
     }
 
     return 0;
@@ -55,7 +99,7 @@ int main(int argc, char* argv[]) {
     int maxChunk = std::stoi(std::string(argv[2]));
     int avgChunk = std::stoi(std::string(argv[3]));
 
-    hydra::chunking::ChunkerAdapter adapter(minChunk, maxChunk, avgChunk);
+    ChunkerAdapter adapter(minChunk, maxChunk, avgChunk);
     std::string fileName;
 
     while (std::getline(std::cin, fileName)) {
@@ -69,8 +113,8 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    while (!adapter.isEmpty()) {
-        std::cout << getChunkSize(adapter.getTerminalChunkRef()) << std::endl;
-    }
+    while (!adapter.isEmpty())
+        std::cout << adapter.getTerminalChunk() << std::endl;
+
     return 0;
 }
